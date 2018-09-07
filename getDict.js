99e61a84e38183d8a65e313dbe79791ff15e7f5d
petershaker <peters.shaker@gmail.com>
2018-09-07 18:08:48 +0200

    get a key & list all

--- getDict.js
+++ getDict.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
 
 module.exports = {
-    getFileData: (fileName) => {
-           fs.readFile(fileName, (err, contents) => {
-               if (err) {
-                   return console.error(err);
-               }
-               // dict = JSON.parse(contents)
-               console.log("Asynchronous read: " + contents);
-           });
-        //    console.log(contents);
-           
-    }
+  getFileData: (fileName, key) => {
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        return console.error(err);
+      } else {
+        if (key) {
+          contents = JSON.parse(data);
+          console.log(`the value of ${key} is ${contents[key]}`);
+        } else {
+          // if list
+          console.log("Asynchronous read: " + data);
+        }
+      }
+    });
+  }
 };
