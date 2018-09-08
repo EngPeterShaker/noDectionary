762b2e5370208c0f2041f20de90e5d41138459d0
petershaker <peters.shaker@gmail.com>
2018-09-08 17:30:13 +0200

    make new file in the first add

--- setDict.js
+++ setDict.js
@@ -1,34 +1,45 @@
 const fs = require("fs");
 const util = require("util");
 
-//const getDict = require("./getDict.js");
+const setFileData = (fileName, type, key, value) => {
+  const readFile = util.promisify(fs.readFile);
 
-module.exports = {
-  setFileData: (fileName, type, key, value) => {
-    //  const dataObj = await getDict.getFileDataSync(fileName);
-    const readFile = util.promisify(fs.readFile);
-    async function getStuff() {
-      return await readFile(fileName, "utf-8");
-    }
-    getStuff().then(data => {
+  fs.exists(fileName, exists => {
+    if (!exists) {
+      console.log("No Dictionaries file found ");
+      let newObj = {};
+      newObj[key] = value;
+
+      fs.writeFileSync(fileName, JSON.stringify(newObj), err => {
+        console.log(err);
+      });
+      console.log("No Problem \n A new file is created");
+    } 
+  });
+
+  async function getStuff() {
+    return await readFile(fileName, "utf-8");
+  }
+  getStuff()
+    .then(data => {
       let newsss = JSON.parse(data);
-
       if (type === "add") {
-        console.log("this is add");
         newsss[key] = value;
       } else if (type === "remove") {
         delete newsss[key];
       } else if (type === "clear") {
         newsss = {};
-        console.log("this is clear");
       }
 
-      fs.writeFileSync(fileName, JSON.stringify(newsss), err => {
+      fs.writeFile(fileName, JSON.stringify(newsss), err => {
         if (err) {
           return console.error(err);
         }
-        console.log("this");
       });
+    })
+    .catch(err => {
+      //console.log(err);
     });
-  }
 };
+// exports function above so that other modules can use it
+module.exports = { setFileData };
