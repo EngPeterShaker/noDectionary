e435bf44211f29f9699ff7a5c8a18778b90d672d
petershaker <peters.shaker@gmail.com>
2018-09-08 17:30:53 +0200

    adding readabilty for list && error handling for get

--- getDict.js
+++ getDict.js
@@ -1,35 +1,34 @@
 const fs = require("fs");
 
-let content;
-module.exports = {
-  getFileData: (fileName, type, key) => {
+  getFileData = (fileName, type, key) => {
     fs.readFile(fileName, (err, data) => {
       if (err) {
-        throw err;
+        console.error(
+          "There's no dictionary file , please set a new key/value pair to initialize it \n"
+        );
+        return console.error(err);
+        // throw err;
       } else {
         // if get
+        contents = JSON.parse(data);
         if (type === "get") {
-          contents = JSON.parse(data);
-          console.log(`the value of ${key} is ${contents[key]}`);
+          key !== undefined
+            ? console.log(`the value of ${key} is ${contents[key]}`)
+            : console.log("no key was provided \n pssst.. Do you mean [list]");
         } else if (type === "list") {
           //if list
-          console.log("Asynchronous read: " + data);
+          console.log(
+            `ALL Dictionary file List : \n ${JSON.stringify(
+              contents,
+              null,
+              "\t"
+            )}`
+          );
         } else {
-          console.log("elese");
           return data;
         }
       }
     });
-  },
-  getFileDataSync: fileName => {
-    function readContent(cb) {
-      fs.readFile(fileName, "utf-8", function(err, content) {
-        if (err) return cb(err);
-        cb(null, content);
-      });
-    }
-    readContent(function(err, content) {
-      return content;
-    });
   }
-};
+// exports function above so that other modules can use it  
+module.exports = { getFileData };
