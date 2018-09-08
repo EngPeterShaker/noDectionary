aceeca57479202aeaa50e72eee370a311acd9bda
petershaker <peters.shaker@gmail.com>
2018-09-08 17:29:40 +0200

    adding spread operator for scalability

--- store.js
+++ store.js
@@ -1,53 +1,41 @@
 const fs = require("fs");
 const process = require("process");
-const path = require("path");
+//const path = require("path");
 const getDict = require("./getDict.js");
 const setDict = require("./setDict.js");
 
 const FILENAME = "dictionary.json";
 // var myPath = path.basename('/foo/bar/baz/asdf/quux.html', '.html');
-// console.log(myPath);
 let arg_array = [];
 
 process.argv.forEach((val, index) => {
   if (index > 1) {
     arg_array.push(val);
-    // console.log(`${index}: ${val}`);
   }
-});
-
-//::Todo::
-fs.exists(FILENAME, exists => {
-  if (!exists) {
-    console.log("new file is created");
-    setDict.setFileData(FILENAME, {});
-  }
-  // return exists ? true : false;
 });
 
 switch (arg_array[0]) {
   case "add":
-    // text = "Today is add";
-    setDict.setFileData(FILENAME, arg_array[0], arg_array[1], arg_array[2]); //todo : spread
+    setDict.setFileData(FILENAME, ...arg_array);
+    console.log(`Successful ${arg_array[0]} process`);
     break;
   case "list":
-    // text = "Today is list";
-    getDict.getFileData(FILENAME, arg_array[0]);
+  getDict.getFileData(FILENAME, ...arg_array);
+    console.log(`Successful ${arg_array[0]} process`);
     break;
   case "get":
-    // text = "Today is get";
-    getDict.getFileData(FILENAME, arg_array[0], arg_array[1]);
+    getDict.getFileData(FILENAME, ...arg_array);
+    console.log(`Successful ${arg_array[0]} process`);
     break;
   case "remove":
-    // text = "Today is remove";
-    setDict.setFileData(FILENAME, arg_array[0], arg_array[1]); //todo : spread
-
+    setDict.setFileData(FILENAME, ...arg_array);
+    console.log(`Successful ${arg_array[0]} process`);
     break;
   case "clear":
-    // text = "Today is clear";
-    setDict.setFileData(FILENAME, arg_array[0]); //todo : spread
+    setDict.setFileData(FILENAME, ...arg_array); //todo : spread
+    console.log(`Successful ${arg_array[0]} process`);
     break;
   default:
-    text = "Looking forward to a newsss trial";
+    console.log(` What's [${arg_array[0]}] ??? \n  I have never heard of that before !! .. would u try again plz :) `);
 }
-console.log(`Successful ${arg_array[0]} process`);
+
